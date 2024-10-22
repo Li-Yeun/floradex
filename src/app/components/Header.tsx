7e2fdfc06da84c3b530435e3aa708f669b0abd75
Li-Yeun <45470843+Li-Yeun@users.noreply.github.com>
2024-10-22 21:36:25 +0200

    added header-camera

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 function Header() {
   return (
-    <div className='h-full touch-none overscroll-none'>
+    <div className='h-full'>
       <div className='border-container fixed -mt-[65vw] aspect-[751/501] w-full'>
         <Image
           src='/images/header-background.png'
@@ -10,6 +10,17 @@ function Header() {
           layout='fill'
           className='-z-20'
         ></Image>
+
+        <div className='flex h-full w-full justify-end'>
+          <div className='relative mr-[2%] aspect-square h-[100%] w-[22%]'>
+            <Image
+              src='/images/header-camera.png'
+              alt='header background'
+              layout='fill'
+              className='z-10 translate-y-[27.5%] object-contain'
+            ></Image>
+          </div>
+        </div>
       </div>
 
       <div className='border-container fixed -mt-[6vw] aspect-[390/62] w-full'>
